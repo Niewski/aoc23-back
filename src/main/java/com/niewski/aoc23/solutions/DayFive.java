3a1eb6d13e8416ec227959d05e277de21d0e3398
Jacob Wisniewski <jacobawisniewski@gmail.com>
2023-12-09 00:28:27 -0500

    works but too slow

--- src/main/java/com/niewski/aoc23/solutions/DayFive.java
+++ src/main/java/com/niewski/aoc23/solutions/DayFive.java
@@ -1,8 +1,6 @@
 package com.niewski.aoc23.solutions;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.niewski.aoc23.dto.IslandAlmanac;
 import com.niewski.aoc23.responses.TwoLongResponse;
@@ -19,52 +17,59 @@ public class DayFive {
     public static long solvePartOne(IslandAlmanac almanac) {
         long result = 0;
         // Iterate through seeds and get the location for each seed
-        for (Long seed: almanac.getSeeds()) {
-            // Create map for relevant list and set destination
-            Map<Long, Long> map = getMap(almanac.getSeedToSoil());
-            long soil = map.getOrDefault(seed, seed);
+        for (Long code: almanac.getSeeds()) {
+            code = findValue(almanac.getSeedToSoil(), code);
+            code = findValue(almanac.getSoilToFertilizer(), code);
+            code = findValue(almanac.getFertilizerToWater(), code);
+            code = findValue(almanac.getWaterToLight(), code);
+            code = findValue(almanac.getLightToTemperature(), code);
+            code = findValue(almanac.getTemperatureToHumidity(), code);
+            code = findValue(almanac.getHumidityToLocation(), code);
 
-            map = getMap(almanac.getSoilToFertilizer());
-            long fertilizer = map.getOrDefault(soil, soil);
-
-            map = getMap(almanac.getFertilizerToWater());
-            long water = map.getOrDefault(fertilizer, fertilizer);
-
-            map = getMap(almanac.getWaterToLight());
-            long light = map.getOrDefault(water, water);
-
-            map = getMap(almanac.getLightToTemperature());
-            long temperature = map.getOrDefault(light, light);
-
-            map = getMap(almanac.getTemperatureToHumidity());
-            long humidity = map.getOrDefault(temperature, temperature);
-
-            map = getMap(almanac.getHumidityToLocation());
-            long location = map.getOrDefault(humidity, humidity);
-            
             // Check if location is less than current result
-            result = result == 0 ? location : Math.min(location, result);
+            result = result == 0 ? code : Math.min(code, result);
         }
         return result;
     }
 
-    public static long solvePartTwo(IslandAlmanac scratchOffs) {
+    // Sooooo sloww
+    public static long solvePartTwo(IslandAlmanac almanac) {
         long result = 0;
-        
-        return result;
-    }
+        int count = almanac.getSeeds().size();
 
-    private static Map<Long, Long> getMap(List<List<Long>> input) {
-        Map<Long, Long> map = new HashMap<Long, Long>();
-        for (List<Long> list : input) {
-            long range = list.get(2);
-            Long source = list.get(1);
-            long destination = list.get(0);
-            for(long i = 0; i < range; i++) {
-                map.put(source + i, destination + i);
+        // Iterate through seeds and get the location for each seed
+        for(int i = 0; i < count / 2; i++) {
+            long start = almanac.getSeeds().get(i);
+            i++;
+            long end = start + almanac.getSeeds().get(i);
+            for(long j = start; j < end; j++) {
+                long code = j;
+                code = findValue(almanac.getSeedToSoil(), code);
+                code = findValue(almanac.getSoilToFertilizer(), code);
+                code = findValue(almanac.getFertilizerToWater(), code);
+                code = findValue(almanac.getWaterToLight(), code);
+                code = findValue(almanac.getLightToTemperature(), code);
+                code = findValue(almanac.getTemperatureToHumidity(), code);
+                code = findValue(almanac.getHumidityToLocation(), code);
+
+                // Check if location is less than current result
+                result = result == 0 ? code : Math.min(code, result);
             }
         }
-        return map;
+
+        return result;
+    }
+
+    private static long findValue(List<List<Long>> lists, long value) {
+        for (List<Long> list : lists) {
+            long range = list.get(2);
+            long source = list.get(1);
+            if(source <= value && value < source + range) {
+                long destination = list.get(0);
+                return destination + (value - source);
+            }
+        }
+        return value;
     }
 
 }
--- src/test/java/com/niewski/aoc23/solutions/DayFiveTest.java
+++ src/test/java/com/niewski/aoc23/solutions/DayFiveTest.java
@@ -78,4 +78,59 @@ public class DayFiveTest {
         // Assert
         assertEquals(1, result); // The smallest seed is returned when all maps are null
     }
+
+    // Solve Part Two
+    @Test
+    public void testSolvePartTwo() {
+        // Arrange
+        List<Long> seeds = Arrays.asList(79L, 14L, 55L, 13L);
+        when(mockAlmanac.getSeeds()).thenReturn(seeds);
+
+        List<List<Long>> stsList = Arrays.asList(Arrays.asList(50L, 98L, 2L), Arrays.asList(52L, 50L, 48L));
+        when(mockAlmanac.getSeedToSoil()).thenReturn(stsList);
+        List<List<Long>> stfList = Arrays.asList(Arrays.asList(0L, 15L, 37L), Arrays.asList(37L, 52L, 2L), Arrays.asList(39L, 0L, 15L));
+        when(mockAlmanac.getSoilToFertilizer()).thenReturn(stfList);
+        List<List<Long>> ftwList = Arrays.asList(Arrays.asList(49L, 53L, 8L), Arrays.asList(0L, 11L, 42L), Arrays.asList(42L, 0L, 7L), Arrays.asList(57L, 7L, 4L));
+        when(mockAlmanac.getFertilizerToWater()).thenReturn(ftwList);
+        List<List<Long>> wtlList = Arrays.asList(Arrays.asList(88L, 18L, 7L), Arrays.asList(18L, 25L, 70L));
+        when(mockAlmanac.getWaterToLight()).thenReturn(wtlList);
+        List<List<Long>> lttList = Arrays.asList(Arrays.asList(45L, 77L, 23L), Arrays.asList(81L, 45L, 19L), Arrays.asList(68L, 64L, 13L));
+        when(mockAlmanac.getLightToTemperature()).thenReturn(lttList);
+        List<List<Long>> tthList = Arrays.asList(Arrays.asList(0L, 69L, 1L), Arrays.asList(1L, 0L, 69L));
+        when(mockAlmanac.getTemperatureToHumidity()).thenReturn(tthList);
+        List<List<Long>> htlList = Arrays.asList(Arrays.asList(60L, 56L, 37L), Arrays.asList(56L, 93L, 4L));
+        when(mockAlmanac.getHumidityToLocation()).thenReturn(htlList);
+        
+        // Act
+        long result = DayFive.solvePartTwo(mockAlmanac);
+
+        // Assert
+        assertEquals(46, result);
+    }
+
+    @Test
+    public void testSolvePartTwoWithEmptySeeds() {
+        // Arrange
+        List<Long> seeds = Arrays.asList();
+        when(mockAlmanac.getSeeds()).thenReturn(seeds);
+
+        // Act
+        long result = DayFive.solvePartTwo(mockAlmanac);
+
+        // Assert
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testSolvePartTwoWithNullMaps() {
+        // Arrange
+        List<Long> seeds = Arrays.asList(1L, 2L, 3L);
+        when(mockAlmanac.getSeeds()).thenReturn(seeds);
+
+        // Act
+        long result = DayFive.solvePartTwo(mockAlmanac);
+
+        // Assert
+        assertEquals(1, result); // The smallest seed is returned when all maps are null
+    }
 }
